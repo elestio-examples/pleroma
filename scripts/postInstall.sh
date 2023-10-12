a8b9d61bb00a6c6c833c53e12f69a581b811441a
AmsellemJoseph <amsellem.joseph@gmail.com>
2023-10-12 17:54:29 +0300

    fix

--- scripts/postInstall.sh
+++ scripts/postInstall.sh
@@ -3,6 +3,6 @@ set -o allexport; source .env; set +o allexport;
 
 #wait until the server is ready
 echo "Waiting for software to be ready ..."
-sleep 150s;
+sleep 250s;
 
 echo 'y' | docker-compose exec -T pleroma ./cli.sh user new admin ${ADMIN_EMAIL} --admin --password ${ADMIN_PASSWORD}
