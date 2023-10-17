e87be7b58efb777f026a4b73cdd1ca6a59dc8c37
Prem chavhan <pchavhan@logiquad.com>
2023-10-17 22:03:48 +0530

    fixes

--- tests/build.sh
+++ tests/build.sh
@@ -1,2 +1,5 @@
 #!/usr/bin/env bash
+rm -f docker-compose.yaml
+mv docker-compose-new.yml docker-compose.yml
+sed -i "s~explodingcamera~ELESTIO~g" start.sh
 docker buildx build . --output type=docker,name=elestio4test/pleroma:latest | docker load
